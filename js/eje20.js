11f9f410dcc82441b9a94984d351ff67c6eabfcf
Carlos <amorescarlos93@hotmail.com>
2019-11-15 15:05:14 +0100

    Metodo animate

---
+++ js/eje20.js
@@ -0,0 +1,23 @@
+// Capitulo 20: Velocidad animacion de las aplicaciones
+// http://gsgd.co.uk/sandbox/jquery/easing/#example
+$(function(){
+	/*$('body').keyup(function(event) {
+		if(event.keyCode == 37){
+			$('.cubo').animate({left: '-=50px'}, 'fast');
+		}else if(event.keyCode == 39){
+			$('.cubo').animate({left: '+=50px'}, 'fast');
+		}else if(event.keyCode == 38){
+			$('.cubo').animate({top: '-=50px'}, 'slow');
+		}else if(event.keyCode == 40){
+			$('.cubo').animate({top: '+=50px'}, 2000);
+		}else{
+			console.log('No ha presionado las teclas de direccion');
+		}
+	});*/
+	/*$('.cubo').click(function(event) {
+		$(this).animate({
+			height: ['+=100', 'easeOutBounce'],
+			width: ['+=100', 'swing']
+		}, 3000);
+	});*/
+});
